e13721e674e1870978317f3d802abcad420bcf9b
Akash Matlani <akash.matlani@dashtechinc.com>
2021-06-25 15:26:46 +0530

    code modification for tabs

--- lib/ui/student/CoolStuffScreen.dart
+++ lib/ui/student/CoolStuffScreen.dart
@@ -38,7 +38,7 @@ class _CoolStuffScreenScreenState extends State<CoolStuffScreen> with SingleTick
 
     var tabHeight = MediaQuery.of(context).size.height * 0.06;
 
-    /*return Scaffold(
+    return Scaffold(
       body: Stack(
         fit: StackFit.expand,
         children: [
@@ -148,31 +148,7 @@ class _CoolStuffScreenScreenState extends State<CoolStuffScreen> with SingleTick
           )
         ],
       ),
-    );*/
-
-    return  MaterialApp(
-      home: DefaultTabController(
-        length: 3,
-        child: Scaffold(
-          appBar: AppBar(
-            bottom: TabBar(
-              tabs: [
-                Tab(icon: Icon(Icons.directions_car)),
-                Tab(icon: Icon(Icons.directions_transit)),
-                Tab(icon: Icon(Icons.directions_bike)),
-              ],
-            ),
-            title: Text('Tabs Demo'),
-          ),
-          body: TabBarView(
-            children: [
-              Icon(Icons.directions_car),
-              Icon(Icons.directions_transit),
-              Icon(Icons.directions_bike),
-            ],
-          ),
-        ),
-      ),
     );
+
   }
 }
